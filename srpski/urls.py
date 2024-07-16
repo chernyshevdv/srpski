8dab157680bb3aaf15b1b794fc1c3968d961c351
dchernyshev <dchernyshev@griddynamics.com>
2024-07-16 13:13:28 +0200

    Change URLs

--- srpski/urls.py
+++ srpski/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('srpski/words/lists/', words_views.list_wordlists, name="words_lists"),
     path('srpski/words/lists/<int:id>/guess', words_views.guess_words_in_list, name="guess_words_in_list"),
     path('srpski/words/lists/<int:id>/show', words_views.show_words_list, name="show_words_list"),
-    path('srpski/theory/cases/', theory_views.cases, name="theory_cases")
+    path('srpski/theory/<str:section>/', theory_views.theory_sections, name="theory_sections")
 ]
